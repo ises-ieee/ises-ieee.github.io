1501ab9d073666714fe4f64952637f6d59622442
sm20251004 <sarajumohanty@gmail.com>
2026-02-15 15:03:33 -0600

    Updated Deployed files

--- IEEE-iSES_Website_Generator/.eleventy.js
+++ IEEE-iSES_Website_Generator/.eleventy.js
@@ -7,11 +7,12 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/get_images.php": "get_images.php" });
 
   return {
+    pathPrefix: process.env.ELEVENTY_PATH_PREFIX || "/",
     dir: {
       input: "src/pages",
       includes: "../_includes",
       data: "../_data",
-      output: path.resolve(__dirname, "../IEEE-iSES_2026_Website")
+      output: "IEEE-iSES_2026_Website"
     }
   };
 };
