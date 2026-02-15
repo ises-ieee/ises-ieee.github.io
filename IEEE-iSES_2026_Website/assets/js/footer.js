062a26c2174c03435e0de8b042fdb852ba523113
sm20251004 <sarajumohanty@gmail.com>
2026-02-15 17:08:34 -0600

    Fixing folder name

---
+++ IEEE-iSES_2026_Website/assets/js/footer.js
@@ -0,0 +1,8 @@
+// footer.js
+function loadFooter() {
+    fetch('../../footer.html')
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById('footer-placeholder').innerHTML = data;
+      });
+}
