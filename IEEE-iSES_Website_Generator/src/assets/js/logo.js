8e5b4625b7c59d90df6af86fac592fdd9a4d69b4
sm20251004 <sarajumohanty@gmail.com>
2026-02-18 18:47:41 -0600

    Re-add files without LFS

--- IEEE-iSES_Website_Generator/src/assets/js/logo.js
+++
@@ -1,7 +0,0 @@
-function loadLogo() {
-    fetch('../../logo.html')
-      .then(response => response.text())
-      .then(data => {
-          document.getElementById('logo-placeholder').innerHTML = data;
-      });
-}
